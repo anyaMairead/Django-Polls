6547d9abd10e7567a7e2db40924a01b8e3cdb02a
Laurence Toal <laurence.toal@gmail.com>
2011-06-18 18:54:53 -0700

    initial commit

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^polls/', include('polls.urls')),
+    (r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^admin/', include(admin.site.urls)),
+)
